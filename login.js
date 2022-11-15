15c4a5b58ceecb32a4e5a90d39fe619fbbba597f
Mirziyod <111650889+Mirziyod-saidov@users.noreply.github.com>
2022-11-15 09:47:11 +0500

    end

---
+++ login.js
@@ -0,0 +1,53 @@
+"use strict";
+const BASE_URL = "https://n36-todolist.herokuapp.com"
+const auth = () => {
+    const username = $('#user').value.trim();
+    const password = $('#password').value.trim();
+
+    const params = {
+        login: username,
+        password: password
+    }
+    console.log(params)
+    fetch(`${BASE_URL}/signup`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(params)
+    }).then((result) => result.json())
+        .then(res => {
+            console.log(res);
+
+
+
+            if (res.message) {
+                $('.modal-toast').innerHTML = `<strong>${res.message}</strong>`;
+                $('.modal-toast').classList.remove('hide-toast');
+                setTimeout(() => {
+                    $('.modal-toast').classList.add('hide-toast')
+                }, 2000)
+            }
+
+            let token = localStorage.getItem('token');
+            if (token) {
+                localStorage.setItem('token', res.token)
+                localStorage.setItem('userName', params.login)
+                $('.modal-toast').classList.remove('alert-danger');
+                $('.modal-toast').classList.add('alert-success');
+                $('.modal-toast').innerHTML = `<strong>${"SUCCESSFULLY LOGGED IN"}</strong>`;
+                $('.modal-toast').classList.remove('hide-toast');
+                setTimeout(() => {
+                    $('.modal-toast').classList.add('hide-toast');
+                    window.location.replace("./index.html")
+                }, 2000)
+
+            }
+        })
+}
+
+$('#login').addEventListener("submit", (e) => {
+    e.preventDefault()
+    console.log("ok")
+    auth()
+})
--- script.js
+++ script.js
@@ -1,123 +1,201 @@
 "use strict";
 
-// -----------------GET USER LIST-----------//
-
+// ------------ GET USER LIST -------------------- //
 function getUser() {
-    fetch('http://localhost:7777/user')
-        .then((response) => response.json())
-        .then((result) => renderData(result))
-
+   fetch('http://localhost:7777/user')
+      .then((response) => response.json())
+      .then((result) => renderData(result))
 }
 
 getUser()
 
+// ------------ GET USER LIST -------------------- //
 
-// -----------------GET USER LIST-----------//
 
-//----------------RENDER FUNCTION-----------------//
+// ----------- RENDER FUNCTION -------------///
 function renderData(data = []) {
 
-    data.length > 0 ? data.forEach((el) => {
-        const tr = createElement('tr', "item", `<td>${el.id}</td>
-        <td>${el.user_name}</td>
-        <td>${el.score}</td>
-        <td> <button class="btn btn-primary" data-edit="${el.id}">EDIT</button> </td>
-        <td> <button class="btn btn-danger" data-del="${el.id}">DELETE</button> </td>`);
+   let averageScore = data.reduce((a, b) => {
+      return (a * 1 + b.score * 1);
+   }, 0)
 
-        $('tbody').appendChild(tr);
+   $('#averager_score').innerHTML = (averageScore / data.length * 1).toFixed(2)
 
-    }) : $('tbody').innerHTML = "User list is empty"
+   data.length > 0 ? data.forEach((el) => {
+      const tr = createElement('tr', "item", ` <td>${el.id}</td>
+    <td>${el.user_name}</td>
+    <td>${el.score}</td> 
+    <td> <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-edit="${el.id}">EDIT</button> </td>
+    <td> <button class="btn btn-danger" data-del="${el.id}">DELETE</button> </td>`);
+      $('tbody').appendChild(tr);
 
+   }) : $('tbody').innerHTML = "USERS LIST EMPTY"
 
 }
 
-//----------------RENDER FUNCTION END-----------------//
+// ----------- RENDER FUNCTION  END -------------///
 
 
+
+// POST METHODS --------------------------------
 const addUser = () => {
-    const userName = $("#userName").value.trim();
-    const userScore = $("#userScore").value.trim();
+   const userName = $("#userName").value.trim();
+   const userScore = $("#userScore").value.trim();
 
-    const params = {
-        user_name: userName,
-        score: userScore
-    }
 
-    if (userScore.length === 0 || userName.length === 0) {
-        // alert('please fill the user name and score');
-        $('.toastify').style.backgroundColor = 'crimson';
-        $('.toastify').innerHTML = `<h5>Not enough info</h5>`
-        $('.toastify').style.transform = 'translateX(0)';
-        setTimeout(() => {
-            $('.toastify').style.transform = 'translateX(200%)';
+   if (userScore.length === 0 || userName.length === 0) {
 
-        }, 1500)
-    }
-    else {
-        $('.toastify').style.backgroundColor = 'lime';
-        $('.toastify').innerHTML = `<h5>Successfully added</h5>`
-        $('.toastify').style.transform = 'translateX(0)';
-        setTimeout(() => {
-            fetch('http://localhost:7777/user', {
-                method: 'POST',
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(params)
+      $('.toastify').style.backgroundColor = "crimson";
+      $('.toastify').innerHTML = `<h5>Ma'lumot yetarli emas!</h5>`
+      $('.toastify').style.transform = 'translateX(0)';
+
+      setTimeout(() => {
+         $('.toastify').style.transform = 'translateX(200%)';
+      }, 1500)
+
+   } else {
+      $('.toastify').style.backgroundColor = "lime";
+      $('.toastify').innerHTML = `<h5>Muaffaqiyatli qo'shildi !</h5>`
+      $('.toastify').style.transform = 'translateX(0)';
+
+      setTimeout(() => {
+         $('.toastify').style.transform = 'translateX(200%)';
+         fetch('http://localhost:7777/user', {
+            method: "POST",
+            headers: {
+               "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+               user_name: userName,
+               score: userScore
             })
-        }, 1500)
-    }
+         })
+
+      }, 2000)
+   }
 }
 
 
 $('#send').addEventListener('submit', () => {
-    addUser()
+
+   addUser()
 })
 
+// POST METHODS --------------------------------------------------------------
 
 
-//-------------------------DELETE FUNCTION-------------//
-
+// ----------------DELETE FUNCTION-------
 
 $('tbody').addEventListener('click', (e) => {
-    if (e.target.classList.contains('btn-danger')) {
-        let id = e.target.getAttribute('data-del');
-        deleteUser(id)
-    }
-
+   if (e.target.classList.contains('btn-danger')) {
+      let id = e.target.getAttribute('data-del');
+      deleteUser(id);
+   }
 })
 
 
 const deleteUser = (id) => {
-    $('.toastify').style.transform = 'translateX(0)'
-    setTimeout(() => {
-        fetch(`http://localhost:7777/user/${id}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({})
-        })
 
-    }, 2000)
+   $('.toastify').style.transform = 'translateX(0)';
+
+
+   setTimeout(() => {
+
+      $('.toastify').style.transform = 'translateX(200%)';
+      fetch(`http://localhost:7777/user/${id}`, {
+         method: "DELETE",
+         headers: {
+            "Content-Type": "application/json"
+         },
+         body: JSON.stringify({})
+      })
+
+   }, 2000)
 }
-//-------------------------DELETE FUNCTION END-------------//
+
+
+// ----------------DELETE FUNCTION   -------
 
 
 $('tbody').addEventListener('click', (e) => {
-    if (e.target.classList.contains('btn-primary')) {
-        let id = e.target.getAttribute('data-edit');
-        editUser(id)
-    }
+
+   if (e.target.classList.contains('btn-primary')) {
+
+      let id = e.target.getAttribute('data-edit');
+      localStorage.setItem('editUser', id);
+
+      fetch(`http://localhost:7777/user/${id}`)
+         .then((res => res.json()))
+         .then((result => setValue(result)))
+         .catch((err) => console.log(err))
+
+   }
+
 
 })
 
-const editUser=(id)=>{
-    fetch(`http://localhost:7777/user/${id}`,{
-        method:'PUT',
-        headers:{
-            'Content-Type': 'application/json'
-        },
-        body:JSON.stringify({user_name:"Farrux",score:"45"})
-    })
+
+
+
+const updateUser = () => {
+
+   let id = localStorage.getItem("editUser")
+
+   let newUser = $('#userEdit').value.trim();
+   let newScore = $('#scoreEdit').value.trim();
+
+
+   if (newUser.length === 0 || newScore.length === 0) {
+      $('.toastify').style.backgroundColor = "crimson";
+      $('.toastify').innerHTML = `<h5>Ma'lumot yetarli emas!</h5>`
+      $('.toastify').style.transform = 'translateX(0)';
+
+      setTimeout(() => {
+         $('.toastify').style.transform = 'translateX(200%)';
+      }, 1500)
+
+   } else {
+      fetch(`http://localhost:7777/user/${id}`, {
+
+         method: "PUT",
+         headers: {
+            "Content-Type": "application/json"
+         },
+         body: JSON.stringify({
+            user_name: newUser,
+            score: newScore
+         })
+      })
+   }
+
 }
+
+
+$('#sendEdit').addEventListener('submit', () => {
+   updateUser()
+})
+
+
+
+function setValue(data) {
+   $('#userEdit').value = data.user_name;
+   $('#scoreEdit').value = data.score;
+}
+
+function logined () {
+   let userName = localStorage.getItem('userName')
+   if (userName) {
+
+      $("#login_user").innerHTML = `${userName}`
+
+   }
+   else{
+      window.location.replace('./login.html')
+   }
+}
+logined()
+
+$("#out").addEventListener("click", ()=>{
+   localStorage.clear()
+   logined()
+})
